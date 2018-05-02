8c7fe765028f2857f6d2eaf15f465a8143c30719
Lisa Dean <lisa@lisadean.net>
2018-05-02 12:22:39 -0400

    first commit

---
+++ goblin.py
@@ -0,0 +1,5 @@
+class Goblin(object):
+    def __init__(self):
+        self.health = 6
+        self.power = 2
+
---
+++ hero.py
@@ -0,0 +1,5 @@
+class Hero(object):
+    def __init__(self):
+        self.health = 10
+        self.power = 5
+
